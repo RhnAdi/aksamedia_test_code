f128d1d9a7efcbfc6f2700dd26c7a871b79817e7
RhnAdi <raihanadinugroho8e29@gmail.com>
2024-08-13 10:31:51 +0700

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+    basePath: "/aksamedia_test_code",
+    output: "export",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
